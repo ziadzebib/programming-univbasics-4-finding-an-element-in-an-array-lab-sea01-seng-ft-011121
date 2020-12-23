8b9ca63ed8d9e7f1e7b5a195ca5117792caad2d9
Ziad Zebib <zebibziad@gmail.com>
2020-12-23 21:40:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   counter = 0
   while counter < array.length do
     if array[counter] == value_to_find
-      return value_to_find
+      return counter
     end
     counter +=1
     end  
