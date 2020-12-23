1d6de17984837a61cfb57fe2c7500bcf2ced8486
Ziad Zebib <zebibziad@gmail.com>
2020-12-23 21:51:47 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   element_index_value = nil
   while counter < array.length do
     if array[counter] == value_to_find
-      element_index_value = value_to_find
+      element_index_value = counter
     end
     counter +=1
   end
