ffffe547453b0217e168b8200b0fcb488fc81018
Ziad Zebib <zebibziad@gmail.com>
2020-12-23 21:38:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,13 @@
 def find_element_index(array, value_to_find)
   counter = 0
-  while counter > array.length do
-    
-    
+  while counter < array.length do
+    if array[counter] == value_to_find
+      return value_to_find
+    end
+    counter +=1
+    end  
+  end
   
-end
+  [11, 12, 13]
+  
+  value to find = 13
